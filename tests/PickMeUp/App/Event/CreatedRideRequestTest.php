ae9c193a479f3c707d263e48fa4e7318044df260
Dominik Jagło <dominik.jaglo@gmail.com>
2016-10-11 22:49:50 +0200

    Added CreatedRideRequestTest

---
+++ tests/PickMeUp/App/Event/CreatedRideRequestTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\PickMeUp\App\Event;
+
+use PickMeUp\App\Event\CreatedRideRequest;
+use PickMeUp\App\Model\Ride;
+use PickMeUp\CQRS\Event\Event;
+
+class CreatedRideRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_it_is_event()
+    {
+        $ride = $this->getMockBuilder(Ride::class)->disableOriginalConstructor()->getMock();
+        self::assertInstanceOf('\PickMeUp\CQRS\Event\Event', new CreatedRideRequest($ride));
+    }
+
+    public function test_it_stores_ride_model()
+    {
+        $ride = $this->getMockBuilder(Ride::class)->disableOriginalConstructor()->getMock();
+        self::assertSame($ride, (new CreatedRideRequest($ride))->getRide());
+    }
+
+    public function test_it_has_name()
+    {
+        self::assertSame('created_ride_request', CreatedRideRequest::getName());
+    }
+}
